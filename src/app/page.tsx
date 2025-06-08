9f03ffe29bbe6e7ebc93d27c61dcfd4138d35adf
Subodh <sohonisubodh@gmail.com>
2025-06-08 20:01:33 +0530

    Entire code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client"
 
-
-import { UserButton } from "@/features/auth/components/user-button";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useMemo, useEffect } from "react";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 import { useRouter } from "next/navigation";
+import { Loader } from "lucide-react";
 
 export default function Home() {
   const { data, isLoading } = useGetWorkspaces();
@@ -28,8 +27,8 @@ export default function Home() {
   }, [workspaceId, isLoading, open, setOpen, router]);
 
   return (
-    <div>
-      <UserButton />
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground"/>
     </div>
   );
 }
